ea0cff58a897e040baf6590c69ce9427f9341e36
mozihao_glb <mozihao_glb@qq.com>
2022-04-15 16:55:01 +0800

    modify MyHandlerException again

--- src/main/java/com/mo/easybuy/utils/MyHandlerException.java
+++ src/main/java/com/mo/easybuy/utils/MyHandlerException.java
@@ -13,13 +13,14 @@ import javax.servlet.http.HttpServletResponse;
  * create 2022-03-04 17:03
  * Description
  */
-//@Component
+@Component
 public class MyHandlerException implements HandlerExceptionResolver {
     @Override
     public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("errorMsg","未知异常");
         modelAndView.setViewName("errorPage");
+        System.out.println("捕获到异常");
         return modelAndView;
     }
 }
